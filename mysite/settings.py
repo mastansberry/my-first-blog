bb7074936951be25d17a46c40aa3fa40af969b93
mastansberry <mstansberry@esu2.org>
2019-01-24 11:30:54 -0600

    changed the HTML for the site attempt 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'disirdbvz%8zsx1=zs+_5%r^-^+9m4&k63#hycuyw4-769do1*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
